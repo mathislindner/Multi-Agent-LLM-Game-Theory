9a0ff5bc9a1d6fc40d169d4329ace570c3d1359f
mathislindner <mathis.lindner@gmail.com>
2024-10-05 19:12:53 +0200

    added simple prompting without langchain

--- agents/custom_agent.py
+++ agents/custom_agent.py
@@ -1,14 +1,24 @@
 
 class baseAgent():
-    def __init__(self, name, personality):
-        self.name = name #name is s.t. it understands who it is in the game history
-        self.personality = personality
-        
-    def intent(self, message):
-        return message
+    name : str = "None"
+    prompt : str = ""
 
-    def decide(self, message):
-        return message
+    def __init__(self, name, prompt):
+        self.name = name
+        self.append_prompt(prompt)
+
+    def append_prompt(self, message):
+        self.prompt += message
+
+class honestAgent(baseAgent):
+    def __init__(self, name):
+        personality_prompt = "You are an honest agent that always tells the truth."
+        super().__init__(name, personality_prompt)
+
+class dishonestAgent(baseAgent):
+    def __init__(self, name):
+        personality_prompt = "You are a dishonest agent that always lies."
+        super().__init__(name, personality_prompt)
 
 import random    
 class randomAgent():
