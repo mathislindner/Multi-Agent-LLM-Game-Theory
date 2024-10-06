8108b28edaebfe03aae024560100ac7024344ca9
mathislindner <mathis.lindner@gmail.com>
2024-10-06 23:56:47 +0200

    test huggingaface

---
+++ models/huggingface_models.py
@@ -0,0 +1,22 @@
+import torch
+from transformers import pipeline
+import os
+
+#right now model not saved locally
+model_id = "meta-llama/Llama-3.1-1B-Instruct"
+
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+outputs = pipe(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
