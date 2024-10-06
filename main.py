195198de0c64bc470bd0ad80695d48b169195d1f
mathislindner <mathis.lindner@gmail.com>
2024-10-06 13:47:59 +0200

    updates on laggraph tries

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from logic.prisoners_dilemma_intent_game_engine import run_game
+#from logic.prisoners_dilemma_intent_game_engine import run_game
+from logic.prisoners_test import launch_game
 import argparse
 def main():
     if args.run:
-        run_game()
+        #run_game()
+        launch_game()
     else:
         print("Please provide the --run flag to run the game")
 
