2b8501ab08e34c2739d685b88351059280aa4dfa
mathislindner <mathis.lindner@gmail.com>
2025-02-14 18:56:11 +0100

    changed gamestate

--- src/run_prisoners_mbti.py
+++ src/run_prisoners_mbti.py
@@ -12,7 +12,7 @@ from src.data.prompts.prisoners_dilemma_prompts import get_personality_from_key_
 
 #inherites from MessagesState but we added better structured output for better readability
 #messages: List[Dict[str, str]] = [] https://dev.to/jamesli/advanced-langgraph-implementing-conditional-edges-and-tool-calling-agents-3pdn
-class PDState(MessagesState):
+class PDState(TypedDict):
     """State for prisonner's dilemma game, includes actions taken and messages exchanged by agents.
 
     Args:
@@ -27,18 +27,6 @@ class PDState(MessagesState):
     agent_2_actions: Annotated[List[str], operator.add]
     current_round: int
     total_rounds: int
-    
-    def __init__(self, game_description_prompt: str, personality_key_1: str, personality_key_2: str, total_rounds: int):
-        self.game_description_prompt = game_description_prompt
-        self.personality_key_1 = personality_key_1
-        self.personality_key_2 = personality_key_2
-        self.agent_1_messages = []
-        self.agent_1_actions = []
-        self.agent_2_messages = []
-        self.agent_2_actions = []
-        self.round = 1
-        self.total_rounds = total_rounds
-        return self
         
 class ActionResponse(BaseModel):
     """Repond with action to take: cooperate or defect."""
@@ -48,42 +36,38 @@ class MessageResponse(BaseModel):
     """Respond with message to send to the other agent."""
     message: str
 
-"""
-@tool
-def take_action(action: Literal["cooperate", "defect"]) -> str:
-    '''Use this to choose if you are cooperating or defecting.'''
-    #TODO: make sure we know who is taking the action
-    return action
-
-@tool
-def send_message(message: str) -> str:
-    '''Use this to send a message to the other agent.'''
-    #TODO: make sure we know who is sending the message
-    return message
-"""
-
 # Define the function that calls the model
 def call_model_action_node(model, agent_name:str):
     def call_model_action(state: PDState) -> PDState:
         #TODO: use state to form a prompt
-        response = model.with_structured_output(ActionResponse).invoke(state["messages"])
+        action_prompt = ""
+        if agent_name == "agent_1":
+            personality_prompt = get_personality_from_key_prompt(state["personality_key_1"])
+        else:
+            personality_prompt = get_personality_from_key_prompt(state["personality_key_2"])
+        game_history = get_game_history_prompt(state["agent_1_messages"], state["agent_1_actions"], state["agent_2_messages"], state["agent_2_actions"], state["current_round"])
+        #call_for_action = {'role': 'system', 'content':'write your action now: '}
+        action_prompt = state["game_description_prompt"] + personality_prompt + game_history
+        response = model.with_structured_output(ActionResponse).invoke(action_prompt)
         #TODO: add to state
         return state
     return call_model_action
 
 def call_model_message_node(model, agent_name:str):
     def call_model_message(state: PDState) -> PDState:
+        message_prompt = ""
         if agent_name == "agent_1":
             personality_prompt = get_personality_from_key_prompt(state["personality_key_1"])
         else:
             personality_prompt = get_personality_from_key_prompt(state["personality_key_2"])
-        
         game_history = get_game_history_prompt(state["agent_1_messages"], state["agent_1_actions"], state["agent_2_messages"], state["agent_2_actions"], state["current_round"])
-        message_prompt = ""
-        #TODO: use state to form a prompt, using agent name and personality and what happened in the games.
-        message_prompt = "" #
+        #call_for_message = {'role': 'system', 'content':'write your message to the other user now: '}
+        message_prompt = state["game_description_prompt"] + personality_prompt + game_history
         response = model.with_structured_output(MessageResponse).invoke(message_prompt)
-        #TODO: addto state
+        if agent_name == "agent_1":
+            state["agent_1_messages"].append(response["message"])
+        else:
+            state["agent_2_messages"].append(response["message"])
         return state
     return call_model_message
 
@@ -97,15 +81,11 @@ def should_continue(state: PDState) -> bool:
 def run_n_rounds_w_com(model_name: str, total_rounds: int, personality_key_1: str, personality_key_2: str) -> None:
     # get models
     model = get_model(model_name)
-    # create agents from strings
-    
-    # create graph from agents and n rounds
     #init graph
-    game_description_prompt = "" #get from data.prompts
-    
+    game_description_prompt = "" #get from data.prompts #say how many total rounds will be played
     graph = StateGraph(PDState) #add state definition
     initial_state = PDState(game_description_prompt = game_description_prompt, personality_key_1 = personality_key_1, personality_key_2 = personality_key_2, total_rounds = total_rounds)
-    graph.add_node("add_init_state", lambda: initial_state)
+    #add nodes
     graph.add_node(f"distribute", lambda x: x)
     graph.add_node(f"message_agent_1", call_model_message_node(model, "agent_1"))
     graph.add_node(f"message_agent_2", call_model_message_node(model, "agent_2"))
@@ -113,9 +93,8 @@ def run_n_rounds_w_com(model_name: str, total_rounds: int, personality_key_1: st
     graph.add_node(f"action_agent_2", call_model_action_node(model, "agent_2"))
     graph.add_node(f"gather", lambda x: x)
     graph.add_node(f"increment", increment_round)
-    
-    graph.add_edge(START, "add_init_state")
-    graph.add_edge("add_init_state", "distribute")
+    #add edges
+    graph.add_edge(START, "distribute")
     graph.add_edge("distribute", "message_agent_1")
     graph.add_edge("distribute", "message_agent_2")
     graph.add_edge("message_agent_1", "action_agent_1")
@@ -134,9 +113,10 @@ def run_n_rounds_w_com(model_name: str, total_rounds: int, personality_key_1: st
             }
         )
     
+    #compile and run
     compiled_graph = graph.compile()
     #print mermaid
     print(compiled_graph.get_graph().draw_mermaid())
-    
+    compiled_graph.invoke({"game_description_prompt":game_description_prompt, "personality_key_1":personality_key_1, "personality_key_2":personality_key_2, "current_round" : 1, "total_rounds" : total_rounds})
     end_state = None
     return end_state
