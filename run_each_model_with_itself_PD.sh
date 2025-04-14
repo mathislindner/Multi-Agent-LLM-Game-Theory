7307573aeda265cfcf69aa5b6652e4df3e43a1e5
mathislindner <mathis.lindner@gmail.com>
2025-04-14 15:06:55 +0200

    deepseek and Anthropic work like this

--- run_each_model_with_itself_PD.sh
+++ run_each_model_with_itself_PD.sh
@@ -2,16 +2,16 @@
 
 personas=("NONE" "ISTJ" "ISFJ" "INFJ" "INTJ" "ISTP" "ISFP" "INFP" "INTP" "ESTP" "ESFP" "ENFP" "ENTP" "ESTJ" "ESFJ" "ENFJ" "ENTJ" "ALTRUISTIC" "SELFISH")
 nr_of_rounds=7
-model_providers=("anthropic" "google_vertexai" "deepseek" "openai") #"anthropic" "google_vertexai"
+model_providers=("deepseek" "anthropic" "google_vertexai" "openai") #"anthropic" "google_vertexai"
 
-model_ids=("claude-3-5-haiku-20241022" "gemini-2.0-flash-lite" "deepseek-chat" "gpt-4o-mini-2024-07-18") # "claude-3-5-sonnet-20241022" "gemini-2.0-flash"
+model_ids=("deepseek-chat" "claude-3-5-haiku-20241022" "gemini-2.0-flash-lite" "gpt-4o-mini-2024-07-18") # "claude-3-5-sonnet-20241022" "gemini-2.0-flash"
+
 
 game_name="prisoners_dilemma"
 
 total_iterations=$(( (${#personas[@]} * (${#personas[@]} + 1)) / 2 * ${#model_ids[@]} ))
 echo "Total iterations: $total_iterations"
 current_iteration=0
-exit
 for m in "${!model_ids[@]}"; do
     model_id="${model_ids[$m]}"
     provider="${model_providers[$m]}"
@@ -20,7 +20,7 @@ for m in "${!model_ids[@]}"; do
             agent_1="${personas[$i]}"
             agent_2="${personas[$j]}"
             current_iteration=$((current_iteration + 1))
-            echo "Iteration $current_iteration of $((total_iterations * ${#model_ids[@]})): $agent_1 vs $agent_2 with $model_id ($provider)"
+            echo "Iteration $current_iteration of $((total_iterations)): $agent_1 vs $agent_2 with $model_id ($provider)"
             python main.py --model_id_1 $model_id --model_id_2 $model_id --model_provider_1 $provider --model_provider_2 $provider --rounds $nr_of_rounds --agent_1_persona "$agent_1" --agent_2_persona "$agent_2" --game_name $game_name
         done
     done
